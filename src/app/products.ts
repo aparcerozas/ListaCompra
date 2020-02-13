8f8150c85b39ead7e2ae7469bbf058e69a8e5aef
aparcerozas <aparcerozas@danielcastelao.org>
2020-02-13 12:39:15 +0100

    Lista de compra horizontal

---
+++ src/app/products.ts
@@ -0,0 +1,22 @@
+export const products = [
+  {
+    name: 'Nintendo Switch',
+    price: 300,
+    description: 'Consola híbrida de Nintendo'
+  },
+  {
+    name: 'PS4 Pro',
+    price: 400,
+    description: 'Consola de sobremesa de Sony'
+  },
+  {
+    name: 'XBOX One X',
+    price: 350,
+    description: 'Consola de sobremesa de Microsoft'
+  },
+  {
+    name: 'Google Stadia',
+    price: 100,
+    description: 'No compres esto'
+  }
+];
